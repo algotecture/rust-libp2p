aa41b0290e341d66a8fca80070d4e6eeaff1e63f
Benjamin Kampmann <ben@gnunicorn.org>
2018-08-20 16:15:09 +0200

    Import Transport Failure story with new `TransportError` and type aliases
     - use `failure` to create `TransportError`-Type with custom Errors
     - adds type aliases for readability `DialResult` and `ListenerResult`
     - implement `TransportError` for new aliases
     - implement new aliases and Error everywhere else
    
    fixes #367

--- dns/src/lib.rs
+++ dns/src/lib.rs
@@ -50,7 +50,7 @@ use multiaddr::{AddrComponent, Multiaddr};
 use std::fmt;
 use std::io::{Error as IoError, ErrorKind as IoErrorKind};
 use std::net::IpAddr;
-use swarm::Transport;
+use swarm::{Transport, ListenerResult, DialResult};
 use tokio_dns::{CpuPoolResolver, Resolver};
 
 /// Represents the configuration for a DNS transport capability of libp2p.
@@ -106,7 +106,7 @@ where
     type Dial = Box<Future<Item = (Self::Output, Self::MultiaddrFuture), Error = IoError>>;
 
     #[inline]
-    fn listen_on(self, addr: Multiaddr) -> Result<(Self::Listener, Multiaddr), (Self, Multiaddr)> {
+    fn listen_on(self, addr: Multiaddr) -> ListenerResult<Self> {
         match self.inner.listen_on(addr) {
             Ok(r) => Ok(r),
             Err((inner, addr)) => Err((
@@ -119,7 +119,7 @@ where
         }
     }
 
-    fn dial(self, addr: Multiaddr) -> Result<Self::Dial, (Self, Multiaddr)> {
+    fn dial(self, addr: Multiaddr) -> DialResult<Self> {
         let contains_dns = addr.iter().any(|cmp| match cmp {
             AddrComponent::DNS4(_) => true,
             AddrComponent::DNS6(_) => true,
@@ -231,7 +231,7 @@ mod tests {
     use futures::future;
     use multiaddr::{AddrComponent, Multiaddr};
     use std::io::Error as IoError;
-    use swarm::Transport;
+    use swarm::{Transport, ListenerResult, DialResult};
     use DnsConfig;
 
     #[test]
@@ -249,11 +249,11 @@ mod tests {
             fn listen_on(
                 self,
                 _addr: Multiaddr,
-            ) -> Result<(Self::Listener, Multiaddr), (Self, Multiaddr)> {
+            ) -> ListenerResult<Self> {
                 unreachable!()
             }
 
-            fn dial(self, addr: Multiaddr) -> Result<Self::Dial, (Self, Multiaddr)> {
+            fn dial(self, addr: Multiaddr) -> DialResult<Self> {
                 let addr = addr.iter().collect::<Vec<_>>();
                 assert_eq!(addr.len(), 2);
                 match addr[1] {
--- libp2p/src/lib.rs
+++ libp2p/src/lib.rs
@@ -48,7 +48,7 @@ pub extern crate libp2p_yamux as yamux;
 
 pub mod simple;
 
-pub use self::core::{Transport, ConnectionUpgrade, PeerId, swarm};
+pub use self::core::{Transport, ConnectionUpgrade, PeerId, swarm, ListenerResult, DialResult};
 pub use self::multiaddr::Multiaddr;
 pub use self::simple::SimpleProtocol;
 pub use self::transport_timeout::TransportTimeout;
@@ -107,7 +107,7 @@ impl Transport for CommonTransport {
     type Dial = <InnerImplementation as Transport>::Dial;
 
     #[inline]
-    fn listen_on(self, addr: Multiaddr) -> Result<(Self::Listener, Multiaddr), (Self, Multiaddr)> {
+    fn listen_on(self, addr: Multiaddr) -> ListenerResult<Self> {
         match self.inner.inner.listen_on(addr) {
             Ok(res) => Ok(res),
             Err((inner, addr)) => {
@@ -118,7 +118,7 @@ impl Transport for CommonTransport {
     }
 
     #[inline]
-    fn dial(self, addr: Multiaddr) -> Result<Self::Dial, (Self, Multiaddr)> {
+    fn dial(self, addr: Multiaddr) -> DialResult<Self> {
         match self.inner.inner.dial(addr) {
             Ok(res) => Ok(res),
             Err((inner, addr)) => {
